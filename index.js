604009b31b56756031a5a202617c4073756f1472
Indra Gunawan <indra@propertyguru.com.sg>
2022-05-26 17:32:42 +0800

    update dependencies

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const core = require('@actions/core');
-const aws = require('aws-sdk')
+const aws = require("@aws-sdk/client-ecr");
 
 async function run() {
   const region = core.getInput('aws-region', { required: true })
@@ -11,11 +11,11 @@ async function run() {
   const newTags = core.getInput('new-tags', { required: true }).replace(/\s+/g, '').split(',')
 
   const getImageParams = { registryId, repositoryName, imageIds: [{ imageTag }]}
-  
+
   let putImageCallback = function(err, result) {
     if (err) {
       if (err.code == 'ImageAlreadyExistsException') {
-        console.log('Image already exists, no action')
+        core.info('Image already exists, no action')
         return
       }
       core.setFailed(err.message)
@@ -25,7 +25,7 @@ async function run() {
       core.debug(result)
     }
   }
-  
+
   let getImageCallback = function(err, result) {
     if (err) {
       core.setFailed(err)
@@ -46,7 +46,7 @@ async function run() {
       })
     }
   }
-  
+
   ecr.batchGetImage(getImageParams, getImageCallback);
 }
 
